e5f93f04d9ab4e7ad6fc6292bdadb6bb0ea0175d
Julia Anisio <julia.an.nogueira@gmail.com>
2025-08-19 01:04:36 -0300

    Game seed

--- src/main/java/com/ju_ns/dslist/entities/Game.java
+++ src/main/java/com/ju_ns/dslist/entities/Game.java
@@ -11,12 +11,20 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
-    @Column(name = "game_year" )
+
+    @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
-    private String imUrl;
+    private String platforms;
+    private Double score;
+
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @Column(name = "short_description",  columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(name = "long_description", columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){}
@@ -25,17 +33,91 @@ public class Game {
                 String title,
                 Integer year,
                 String genre,
-                String platform,
-                String imUrl,
+                String platforms,
+                Double score,
+                String imgUrl,
                 String shortDescription,
                 String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
-        this.imUrl = imUrl;
+        this.platforms = platforms;
+        this.score = score;
+        this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
         this.longDescription = longDescription;
     }
 
